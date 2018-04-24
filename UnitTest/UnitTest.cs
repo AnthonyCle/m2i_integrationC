7774a3ecb62c4804478a4e1a77d55495a5bc4a12
Anthony <anthony-cleradin@hotmail.fr>
2018-04-24 11:07:52 +0200

    v2

---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Biblio_IntegratC;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+
+        [TestMethod]
+        public void TestInt()
+        {
+            var result = Integration.GetInt("10");
+            Assert.AreEqual(10, result);
+        }
+    }
+}
